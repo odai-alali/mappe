3946405de9cbc6d13ea49169b6f77531abd62c5a
MahmoudTamaa <mahmoudtamaa@hotmail.com>
2020-05-09 13:49:19 +0200

    fix: change base

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -2,7 +2,7 @@ const config = require('../config.json');
 module.exports = {
   title: config.title,
   description: config.description,
-  base: "/",
+  base: "/mappe/",
   themeConfig: {
     logo: config.logo,
     footer: config.footer,
